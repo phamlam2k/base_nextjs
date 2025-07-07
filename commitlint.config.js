038ac3c13c6bdb245219b5fb6b149a3c8839026e
lam-pnm <lam.pnm@savvycomsoftware.com>
2025-07-07 20:47:41 +0700

    feat: test code

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,2 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    //   TODO Add Scope Enum Here
-    // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
-    "type-enum": [
-      2,
-      "always",
-      [
-        "feat",
-        "fix",
-        "docs",
-        "chore",
-        "style",
-        "refactor",
-        "ci",
-        "test",
-        "revert",
-        "perf",
-        "vercel",
-      ],
-    ],
-  },
-};
+/* eslint-disable import/no-anonymous-default-export */
+export default { extends: ["@commitlint/config-conventional"] };
